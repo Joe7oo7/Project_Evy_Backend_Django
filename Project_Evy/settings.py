f1aa97b0fb03e884265d05eb5e7025f56de7eeb4
Josephrex A <josephrexa@gmail.com>
2024-10-14 15:58:39 +0530

    Update settings.py

--- Project_Evy/settings.py
+++ Project_Evy/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%k&v)d!77tm-wi7muhf-0467m7etajp4(6-l@1uqd($5^uw(7d'
+SECRET_KEY = 'YOUR SECRET KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
